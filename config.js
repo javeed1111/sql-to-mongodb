919a0995a29f672da09a6f4b403007975640c3c2
javeedfedal <103515308+javeed1111@users.noreply.github.com>
2024-05-31 19:00:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-
-    sqlConnectionString: "name="ATSTravel" connectionString="Data Source=104.211.182.161;Database=ATSPrelive3;Integrated Security=false;User ID=ganit;Password=GCVhvj534cfg;MultipleActiveResultSets=True" providerName="System.Data.SqlClient", // Insert your connection string here.
+    sqlConnectionString: "Data Source=104.211.182.161;Database=ATSPrelive3;Integrated Security=false;User ID=ganit;Password=GCVhvj534cfg;MultipleActiveResultSets=True", // Correct connection string format.
     mongoConnectionString: "mongodb://localhost:27017", // This puts the resulting database in MongoDB running on your local PC.
     targetDatabaseName: "target-database", // Specify the MongoDB database where the data will end up.
     skip: [
@@ -9,3 +8,14 @@ module.exports = {
     ],
     remapKeys: true // Set this to false if you want to leave table keys as they are, set to true to remap them to MongoDB ObjectId's.
 };
+// module.exports = {
+
+//     sqlConnectionString: "name="ATSTravel" connectionString="Data Source=104.211.182.161;Database=ATSPrelive3;Integrated Security=false;User ID=ganit;Password=GCVhvj534cfg;MultipleActiveResultSets=True" providerName="System.Data.SqlClient", // Insert your connection string here.
+//     mongoConnectionString: "mongodb://localhost:27017", // This puts the resulting database in MongoDB running on your local PC.
+//     targetDatabaseName: "target-database", // Specify the MongoDB database where the data will end up.
+//     skip: [
+//         "sql-table-to-skip-1", // Add the tables here that you don't want to replicate to MongoDB.
+//         "sql-table-to-skip-2"
+//     ],
+//     remapKeys: true // Set this to false if you want to leave table keys as they are, set to true to remap them to MongoDB ObjectId's.
+// };
